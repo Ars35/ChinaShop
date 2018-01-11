2d08c1c5105a015a52539b5660c001aecf6b18f0
Roman Trekhlebov <xaero_ol@mail.ru>
2018-01-12 00:24:51 +0300

    validation done

---
+++ ChinaShop/Views/ValidatedTextField.swift
@@ -0,0 +1,21 @@
+//
+//  ValidatedTextField.swift
+//  ChinaShop
+//
+//  Created by Roman Trekhlebov on 11.01.2018.
+//  Copyright © 2018 Arseniy Arseniev. All rights reserved.
+//
+
+import UIKit
+
+class ValidatedTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
