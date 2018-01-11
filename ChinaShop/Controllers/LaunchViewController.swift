79d125734e78195a3e5af919bc88a70dac57a55b
Roman Trekhlebov <xaero_ol@mail.ru>
2018-01-11 15:07:32 +0300

    Validating need params

---
+++ ChinaShop/Controllers/LaunchViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LaunchViewController.swift
+//  ChinaShop
+//
+//  Created by Roman Trekhlebov on 07.01.2018.
+//  Copyright © 2018 Arseniy Arseniev. All rights reserved.
+//
+
+import UIKit
+
+class LaunchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
