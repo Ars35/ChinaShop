8c37395eec555f319bcf52336c691c085e5d4954
Roman Trekhlebov <xaero_ol@mail.ru>
2018-01-12 03:16:03 +0300

    LeftBtn tested

---
+++ ChinaShop/Utilities/KartItem.swift
@@ -0,0 +1,9 @@
+//
+//  KartItem.swift
+//  ChinaShop
+//
+//  Created by Roman Trekhlebov on 12.01.2018.
+//  Copyright © 2018 Arseniy Arseniev. All rights reserved.
+//
+
+import Foundation
---
+++ ChinaShop/Views/KartView.swift
@@ -0,0 +1,21 @@
+//
+//  KartView.swift
+//  ChinaShop
+//
+//  Created by Roman Trekhlebov on 12.01.2018.
+//  Copyright © 2018 Arseniy Arseniev. All rights reserved.
+//
+
+import UIKit
+
+class KartView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
